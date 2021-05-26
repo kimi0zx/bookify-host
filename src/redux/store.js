458a60fce36693c823eb3223f26df1fd4a849a09
kimi0zx <kimi0zx@gmail.com>
2021-05-26 18:14:47 +0530

    Remove redux store middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,34 +1,29 @@
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import loggedReducer from './reducers/isLogged';
-import menuReducer from './reducers/isMenu';
-import userState from './reducers/userState';
-import bookid from './reducers/bookid';
-import isAddrev from './reducers/isAddrev'
-import addIamge from './reducers/addIamge'
+import loggedReducer from "./reducers/isLogged";
+import menuReducer from "./reducers/isMenu";
+import userState from "./reducers/userState";
+import bookid from "./reducers/bookid";
+import isAddrev from "./reducers/isAddrev";
+import addIamge from "./reducers/addIamge";
 
-
-const initialState = {}; 
+const initialState = {};
 
 const middleware = [thunk];
 
 const reducers = combineReducers({
-    isLogged : loggedReducer,
-    isMenu : menuReducer,
-	userState : userState , 
-	bookid : bookid ,
-	isAddrev : isAddrev,
-	addImage : addIamge,
+	isLogged: loggedReducer,
+	isMenu: menuReducer,
+	userState: userState,
+	bookid: bookid,
+	isAddrev: isAddrev,
+	addImage: addIamge,
 });
-
 
 const store = createStore(
 	reducers,
 	initialState,
-	compose(
-		applyMiddleware(...middleware),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+	applyMiddleware(...middleware)
 );
 
 export default store;
